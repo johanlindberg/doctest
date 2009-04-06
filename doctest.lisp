e34cde7a3d5110e3dbabfbdcb2bf4722370e8287
Johan Lindberg <johan@pulp.se>
2009-04-06 22:46:51 +0200

    Lots of refactoring and added functionality to test conditions.

--- doctest.lisp
+++ doctest.lisp
@@ -1,85 +1,124 @@
 ;;; doctest
 
-(defun test-function (function &key (output nil))
-  "test-function extracts and tests code snippets embedded in the documentation
+(defpackage :doctest
+  (:use :cl)
+  (:export :test-file
+	   :test-function))
+(in-package :doctest)
+
+(defun whitespace-p (c)
+  "Returns T if <c> is a whitespace character, otherwise NIL."
+
+  (or (equal #\Space c)
+      (equal #\Tab c)
+      (equal #\Newline c)))
+
+(defun run-doctests (docstring output)
+  "Run-doctests is used by test-function and test-file to perform the actual
+   work. It returns the number of tests failed and passed and prints to
+   <output>."
+
+  (let ((tests-passed 0)
+	(tests-failed 0))
+    (when docstring
+      (do ((c (read-char docstring)
+	      (read-char docstring nil 'EOF)))
+	  ((eq c 'EOF))
+	(when (and (equal #\> c)
+		   (equal #\> (read-char docstring))
+		   (whitespace-p (peek-char nil docstring)))
+	  (let* ((signaled-condition 'nil)
+		 (test-form (read docstring))
+		 (expected-results (list (read docstring)))
+		 (test-results (multiple-value-list
+				(handler-case (eval test-form)
+				  (condition (co) (progn
+						    (setf signaled-condition t)
+						    co))))))
+	    (if signaled-condition
+		(let ((test-condition (first test-results))
+		      (expected-condition (first expected-results)))
+		  (if (typep test-condition expected-condition)
+		      (incf tests-passed)
+		      (progn
+			(incf tests-failed)
+			(format output "~&~A signaled ~A, expected ~A.~%" test-form test-condition expected-condition))))
+		(if (equal test-results expected-results)
+		    (incf tests-passed)
+		    (progn
+		      (incf tests-failed)
+		      (format output "~&~A returned~{ ~A~}, expected~{ ~A~}.~%" test-form test-results expected-results))))))))
+    (values tests-failed tests-passed)))
+
+(defun test-function (function &key (output-stream nil))
+  "Test-function extracts and tests code snippets embedded in the documentation
    string of <function>. It returns the number of tests passed and failed and
-   prints a description to <output>.
+   prints a description to <output-stream>.
 
    In order to have a code snippet evaluated as a doctest it must be preceded by
-   3 '>' characters. That will cause the next form to be read and evaluated, and
-   the next N forms after that to be read (but not evaluated). Where N is the
-   number of values the form returns.
+   two '>' characters followed by whitespace. That combination will cause the
+   next form to be read and evaluated, and the next form after that to be read
+   (but not evaluated).
 
    Here is the simplest possible example:
-   >>> 1 ; You can use comments to clarify
+   >> 1 ; NOTE! You can use comments to clarify!
    1
 
-   And here is one with more than one return value.
-   >>> (values 1 2)
-   1
-   2
-   
-   Note that newlines and other whitespace (including comments) doesn't
-   particularly matter. We could have added the test >>> 1 1 more or less
-   anywhere within the docstring.
+   If you excpect more than one value you should wrap it in a multiple-value-
+   list to create one form.
 
-   Another thing to keep in mind is that results are only compared against as
-   many forms as it returns, so this test:
-   >>> (values 1 2)
-   1
-   2
-   3
+   >> (multiple-value-list (values 1 2))
+   (1 2)
 
-   will pass because 3 is considered text and ignored.
+   NOTE! Newlines and other whitespace (including comments) doesn't particularly
+   matter. We could just as well have written >> (multiple-value-list (values 1
+   2)) (1 2) instead.
 
-   Here is a slightly more complicated example:
-   >>> (defun foo (x)
-         (* x x))
-   FOO
-   >>> (foo 1) 1
-   >>> (foo 2) 4
+   If you test a function that doesn't have a documentation string, test-
+   function will return NIL.
+   >> (defun sqr (x)
+        (* x x))
+   SQR
+   >> (test-function #'sqr)
+   NIL
 
-   Since foo doesn't have any doctests specified, test-function will return 0 0.
-   >>> (test-function #'foo) 0 0
+   If you need to test that a function signals a condition for certain inputs
+   you can use the name of the condition as the expected return value.
+   >> (sqr 'x)
+   type-error
 
-   Let's specify a documentation string for foo and add some doctests to it.
-   >>> (defun foo (x)
-         \"Foo returns x squared.
+   If we add a documentation string for sqr with some doctests, we can verify
+   that tests can fail as well.
+   >> (defun sqr (x)
+        \"Returns <x> squared.
 
-           >>> (foo 1) 1
-           >>> (foo 2) 4
+          This test will fail:
+          >> (sqr 3) 3\"
+        (* x x))
+   SQR
 
-           This test will fail:
-           >>> (foo 3) 3\"
-         (* x x))
-   FOO
-   >>> (test-function #'foo) 1 2"
-  (let ((count 0)
-	(tests-passed 0)
-	(tests-failed 0))
-    (when (documentation function 'function)
-      (with-input-from-string (docstring (documentation function 'function))
-	(do ((c (read-char docstring)
-		(read-char docstring nil 'EOF)))
-	    ((eq c 'EOF))
+   Testing foo with test-function should now return 1 and 0.
+   >> (multiple-value-list (test-function #'sqr))
+   (1 0)"
 
-	  (if (equal #\> c)
-	      (progn
-		(incf count)
-		(when (eq count 3)
-		  (let* ((test-form (read docstring))
-			 (test-result (multiple-value-list (eval test-form)))
-			 (expected-result (mapcar #'(lambda (x)
-						      (declare (ignore x))
-						      (read docstring))
-						  test-result)))
-		    (if (equal test-result expected-result)
-			(incf tests-passed)
-			(progn
-			  (incf tests-failed)
-			  (format output "~&~A returned~{ ~A~}, expected~{ ~A~}.~%" test-form test-result expected-result))))
-		  (setf count 0)))
-	      (setf count 0)))))
+  (when (documentation function 'function)
+    (let ((function-name (third (multiple-value-list (function-lambda-expression function)))))
+      (multiple-value-bind (tests-failed tests-passed)
+	  (with-input-from-string (docstring (documentation function 'function))
+	    (run-doctests docstring output-stream))
+	(print-results function-name 'function output-stream tests-failed tests-passed)))))
 
-    (format output "~&Results for #'~A: ~D of ~D failed.~%" (function-name function) tests-failed (+ tests-failed tests-passed))
-    (values tests-failed tests-passed)))
+(defun test-file (filename &key (output-stream nil))
+  "test-file extracts and tests code snippets in the contents of <function>. It
+   returns the number of tests passed and failed and prints a description to
+   <output-stream>."
+
+    (multiple-value-bind (tests-failed tests-passed)
+	(with-open-file (docstring filename :direction :input)
+	  (run-doctests docstring output-stream))
+      (print-results filename 'file output-stream tests-failed tests-passed)))
+
+(defun print-results (test-name test-type output-stream tests-failed tests-passed)
+  (format output-stream "~&Results for ~A (~A): ~D of ~D failed.~%" test-name test-type tests-failed (+ tests-failed tests-passed))
+  (values tests-failed tests-passed))
+
